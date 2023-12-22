1b0a8e5156dd8d2751af9f33674ef1c24e348840
Daryl Kevin <daryl.galvez@outlook.com>
2023-12-22 19:43:04 +0800

    upload clouduploader.sh

---
+++ clouduploader.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo '[X] No arguments supplied'
+    echo '[!] Usage: ./clouduploader <container-name> <filename1>'
+    exit 1
+elif [[ $# -eq 1 ]]; then
+    echo '[!] Too few arguments!'
+    echo '[!] Usage: ./clouduploader <container-name> <filename1>'
+    exit 1
+elif [[ $# -ge 3 ]]; then
+    echo '[!] Too many arguments'
+    echo '[!] Usage: ./clouduploader <container-name> <filename1>'
+    exit 1
+elif [[ ! -f $2 ]]; then
+    echo '[X] The file does not exist!'
+    exit 1
+else
+    does_exist=$(az storage blob exists --container-name $1 --name $2 --auth-mode login | grep exists)
+    if [[ $does_exist == *"true"* ]]; then
+        echo '[!] The file with the same name already exists in your storage.'
+        exit 1
+    else
+        az storage blob upload --container-name $1 -f $2 --auth-mode login
+        echo '[✔] File successfully uploaded!'
+        az storage blob list --container-name $1 --output table --auth-mode login
+        exit 1
+    fi
+    exit 1
+fi
